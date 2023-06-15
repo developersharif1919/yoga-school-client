824d8a2d3b458005335e4045a4fe3c5e52ce7c80
developersharif1919 <developersharif1919@gmail.com>
2023-06-15 08:43:23 +0600

    Complete Dark Mode Setting

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,25 +1,23 @@
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
             <div className="footer p-10 bg-base-200 text-base-content">
-                <div>
-                     <h2>Yoga School</h2>
+                <div style={{height:'100px'}} className="w-[200px] mx-auto">
+                     <img style={{height:'100px'}} src='https://i.ibb.co/2kKMBx1/download.jpg' alt="" />
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title">About Us</span>
+                    <Link to='/classes'>Classes</Link>
+                    <Link to='/Instructors'>Instructors</Link>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span className="footer-title">Socail Links</span>
+                    <a className="link link-hover">Face Book</a>
+                    <a className="link link-hover">Linkedin</a>
+                    <a className="link link-hover">Twitter</a>
                 </div>
                 <div>
                     <span className="footer-title">Legal</span>
